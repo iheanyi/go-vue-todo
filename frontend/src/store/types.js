fee62c368898044828df6e8375e5f03b965aa291
Iheanyi Ekechukwu <iheanyi@digitalocean.com>
2017-06-18 21:54:15 -0400

    Remove unused files, not using VueX anymore.

--- frontend/src/store/types.js
+++
@@ -1,5 +0,0 @@
-export const FETCH_TODOS = 'FETCH_TODOS';
-export const CREATE_TODO = 'CREATE_TODO';
-export const GET_TODO = 'GET_TODO';
-export const UPDATE_TODO = 'UPDATE_TODO';
-export const DELETE_TODO = 'DELETE_TODO';
