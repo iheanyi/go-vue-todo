ef5df881cb4425b30a6f44ddbca5d9740250be67
Iheanyi Ekechukwu <iheanyi@digitalocean.com>
2017-06-18 21:59:21 -0400

    Move Router logic into router.go

--- api/api.go
+++ api/api.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/gorilla/mux"
 	"github.com/iheanyi/go-vue-todo/db"
 )
 
@@ -72,17 +71,6 @@ func IndexHandler() func(w http.ResponseWriter, r *http.Request) {
 	}
 
 	return http.HandlerFunc(fn)
-}
-
-func NewRouter(s *APIService) *mux.Router {
-	r := mux.NewRouter()
-	api := r.PathPrefix("/api").Subrouter()
-	api.HandleFunc("/todos", s.ListTodos).Methods("GET")
-	api.HandleFunc("/todos", s.CreateTodo).Methods("POST")
-	//s.HandleFunc("/todos/{id}").Methods("GET", "POST")
-	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./frontend/dist/static"))))
-	r.PathPrefix("/").HandlerFunc(IndexHandler())
-	return r
 }
 
 func renderJSON(w http.ResponseWriter, res interface{}, code int) {
--- api/router.go
+++ api/router.go
@@ -1,1 +1,18 @@
 package api
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func NewRouter(s *APIService) *mux.Router {
+	r := mux.NewRouter()
+	api := r.PathPrefix("/api").Subrouter()
+	api.HandleFunc("/todos", s.ListTodos).Methods("GET")
+	api.HandleFunc("/todos", s.CreateTodo).Methods("POST")
+	//s.HandleFunc("/todos/{id}").Methods("GET", "POST")
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./frontend/dist/static"))))
+	r.PathPrefix("/").HandlerFunc(IndexHandler())
+	return r
+}
