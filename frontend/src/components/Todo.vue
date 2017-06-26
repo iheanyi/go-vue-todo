baad2ce211b65fa26c7a2714b3341ec72185c7ef
Iheanyi Ekechukwu <iheanyi@digitalocean.com>
2017-06-26 12:53:02 -0400

    Rename initialTodo to editTodo

--- frontend/src/components/Todo.vue
+++ frontend/src/components/Todo.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="bb pa3 b--light-gray todo-item">
-    <span class="b f6">{{ todo.createdAt | datetime }}</span>
-    <span class="db b f6">Last Updated: {{ todo.updatedAt | datetime }}</span>
+    <span class="db mb2 b f6">{{ editTodo.createdAt | datetime }}</span>
+    <span class="db gray f6">Updated: {{ editTodo.updatedAt | datetime }}</span>
     <div v-if="isEditing">
-      <input v-focus type="text" v-model="initialTodo.description" class="outline-transparent ba mv2 b--light-gray bw1 w-100 black bold pa2" />
+      <input v-focus type="text" v-model="editTodo.description" class="outline-transparent ba mv2 b--light-gray bw1 w-100 black bold pa2" />
 
       <div class="mb2">
         <button @click="onSaveClick" class="pointer bg-dark-blue bn dim dib white pv2 ph3 mr2">Save</button>
@@ -11,7 +11,7 @@
       </div>
     </div>
     <div v-else class="pv2">
-      {{ initialTodo.description }}
+      {{ editTodo.description }}
     </div>
     <div class="f6">
       <button @click="onRemoveClick(todo.id)" class="pointer pa0 bg-transparent bn blue underline dib mr2">Delete</button>
@@ -29,7 +29,7 @@
     data() {
       return {
         isEditing: false,
-        initialTodo: {
+        editTodo: {
           ...this.todo,
         },
         cachedTodo: null,
@@ -39,18 +39,18 @@
       onEditClick() {
         this.isEditing = true;
         this.cachedTodo = {
-          ...this.initialTodo,
+          ...this.editTodo,
         };
       },
       onCancelClick() {
         this.isEditing = false;
-        this.initialTodo = {
+        this.editTodo = {
           ...this.cachedTodo,
         };
       },
       async onSaveClick() {
-        const response = await axios.put(`/api/todos/${this.todo.id}`, this.initialTodo);
-        this.initialTodo = response.data;
+        const response = await axios.put(`/api/todos/${this.todo.id}`, this.editTodo);
+        this.editTodo = response.data;
         this.isEditing = false;
       },
     },
